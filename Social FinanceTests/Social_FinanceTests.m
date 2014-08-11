e864fd61eb8bf5c51bf4644d6c930d416dcbc4fc
mtel <mtel@mteldeMac-mini-2.local>
2014-08-11 10:24:12 +0800

    Initial Commit

---
+++ Social FinanceTests/Social_FinanceTests.m
@@ -0,0 +1,34 @@
+//
+//  Social_FinanceTests.m
+//  Social FinanceTests
+//
+//  Created by mtel on 14-8-11.
+//  Copyright (c) 2014年 Mtel. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Social_FinanceTests : XCTestCase
+
+@end
+
+@implementation Social_FinanceTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
